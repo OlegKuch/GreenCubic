1627446f95de753a786834d0e40181f82aa0ffba
Oleg <49838022+OlegKuch@users.noreply.github.com>
2020-07-04 11:50:02 +0300

    New building
    Now game builds in a lot of files instead of one big .exe

--- build_exe.sh
+++ build_exe.sh
@@ -1,5 +1,10 @@
 rm -rfv greencubic_exe/data
+pyinstaller -w -i data/icon.ico --distpath greencubic_exe GreenCubic.py
 cp -rv data greencubic_exe/data
 cp -rvu WhatsNew.txt greencubic_exe/WhatsNew.txt
-pyinstaller -F -w -i data/icon.ico --distpath greencubic_exe GreenCubic.py
+cp -rv dist/GreenCubic/* greencubic_exe
+cp -rvu settings.ini greencubic_exe/settings.ini
+rm -rfv greencubic_exe/GreenCubic
 rm GreenCubic.spec
+rm -rfv dist/GreenCubic
+
